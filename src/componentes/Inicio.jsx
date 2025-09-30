9741d4f3001fe8325de8b6ce285c9bd334a9448f
Francisco Michelazzo <400974@sistemas.frc.utn.edu.ar>
2025-09-30 17:02:56 -0300

    nada

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import InicioAdmin from './InicioAdmin'
-import InicioCliente from './InicioCliente'
 import SelectorDiseno from './SelectorDiseno'
 import InicioDisenador from './InicioDisenador'
 
@@ -8,7 +7,6 @@ export default function Inicio({ tipoUsuario }) {
     return (
         <div>
             {tipoUsuario === 'admin' && <InicioAdmin></InicioAdmin>}
-            {tipoUsuario === 'cliente' && <InicioCliente></InicioCliente>}
             {tipoUsuario === 'disenador' && <InicioDisenador></InicioDisenador>}
             {tipoUsuario === 'cliente' && <SelectorDiseno></SelectorDiseno>}
         </div>
